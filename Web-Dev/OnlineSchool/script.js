099ff916f6390be29dc1c749e4911f71d69bc6ec
ColMaluleke <108086573+ColMaluleke@users.noreply.github.com>
2024-05-31 12:31:44 +0200

    Reworked The Layout of index.html

--- Web-Dev/OnlineSchool/script.js
+++ Web-Dev/OnlineSchool/script.js
@@ -1,5 +1,19 @@
+// script.js
+
+// Reload the page when the "Home" link is clicked
 const homeLink = document.getElementById("home-link");
 
-  homeLink.addEventListener("click", function() {
-    window.location.reload();
+homeLink.addEventListener("click", function() {
+  window.location.reload();
+});
+
+// Toggle the display of the navigation links when the hamburger menu is clicked
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav-links ul');
+
+  hamburgerMenu.addEventListener('click', function() {
+    navLinks.classList.toggle('show');
+    hamburgerMenu.classList.toggle('active');
   });
+});
