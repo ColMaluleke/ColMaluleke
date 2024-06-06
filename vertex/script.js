9cdedd7bd8fa70908d49b1c4640a935a7683af28
ColMaluleke <108086573+ColMaluleke@users.noreply.github.com>
2024-06-06 17:09:55 +0200

    Back to Vertex

--- vertex/script.js
+++ vertex/script.js
@@ -1,3 +1,10 @@
-function exploreMore() {
-    window.location.href = "#services";
-}
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav-links ul');
+
+  hamburgerMenu.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default behavior (optional)
+    navLinks.classList.toggle('show');
+    hamburgerMenu.classList.toggle('active');
+  });
+});
