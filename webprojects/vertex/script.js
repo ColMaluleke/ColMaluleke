e1a4bb8acb36e5a674564e61f114f797f1d5114b
ColMaluleke <108086573+ColMaluleke@users.noreply.github.com>
2024-07-18 14:56:48 +0200

    Update and Clean Up

--- webprojects/vertex/script.js
+++ webprojects/vertex/script.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const hamburgerMenu = document.querySelector('.hamburger-menu');
   const navLinks = document.querySelector('.nav-links ul');
 
-  hamburgerMenu.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default behavior (optional)
+  // Hide navigation links initially
+  navLinks.style.display = 'none'; 
+
+  hamburgerMenu.addEventListener('click', function() {
     navLinks.classList.toggle('show');
     hamburgerMenu.classList.toggle('active');
   });
